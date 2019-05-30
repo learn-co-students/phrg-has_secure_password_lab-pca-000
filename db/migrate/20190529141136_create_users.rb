3b556ca186e9ace59b0583ea08e9a2b4e6281494
Leila Weiss <leila.weiss@powerhrg.com>
2019-05-30 08:53:12 -0400

    Done.

---
+++ db/migrate/20190529141136_create_users.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password_digest
+    end
+  end
+end
